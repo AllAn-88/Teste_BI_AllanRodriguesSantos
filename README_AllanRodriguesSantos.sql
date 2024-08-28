8e4d52e0f33de3585555d03bb4f52812da90e5f5
AllAn-88 <rodriguessallan@yahoo.com.br>
2024-08-28 18:01:22 -0300

    Add files via upload

---
+++ README_AllanRodriguesSantos.sql
@@ -0,0 +1,9 @@
+
+
+/* LINGUAGEM UTILIZADA: SQL 
+
+As consultas foram realizadas em querys simples e curtas.
+Acredito que as consultas atendam as questões
+Os conceitos solicitados foram aplicados 
+
+*/
